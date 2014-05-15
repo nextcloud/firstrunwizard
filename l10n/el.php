cfb1bf35fad1cebb1f2e9e553d28e23ee71dfecc
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-15 00:24:27 -0400

    [tx-robot] updated from transifex

--- l10n/el.php
+++ l10n/el.php
@@ -1,7 +1,9 @@
 <?php
 $TRANSLATIONS = array(
+"Welcome to %s" => "Καλωσήρθατε στο %s",
 "Your personal web services. All your files, contacts, calendar and more, in one place." => "Οι προσωπικές υπηρεσίες ιστού. Όλα τα αρχεία, οι επαφές, το ημερολόγιό σας και πολλά άλλα, σε μια τοποθεσία.",
 "Get the apps to sync your files" => "Λήψη των εφαρμογών για συγχρονισμό των αρχείων σας",
+"Connect your desktop apps to %s" => "Συνδέστε τις εφαρμογές του υπολογιστή σας με το %s",
 "Connect your Calendar" => "Συνδέστε το Ημερολόγιό σας",
 "Connect your Contacts" => "Συνδέστε τις Επαφές σας",
 "Access files via WebDAV" => "Πρόσβαση στα αρχεία μέσω WebDAV",
