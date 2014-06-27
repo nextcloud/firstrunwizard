b9a1cb91c49f826bb491651d8595e679cbf68817
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-06-27 00:24:27 -0400

    [tx-robot] updated from transifex

--- l10n/eo.php
+++ l10n/eo.php
@@ -1,11 +1,14 @@
 <?php
 $TRANSLATIONS = array(
+"Welcome to %s" => "Bonvenon al %s",
 "Your personal web services. All your files, contacts, calendar and more, in one place." => "Viaj personaj TTT-servoj. Ĉiuj viaj dosieroj, kontaktoj, kalendaro kaj pli, en unu loko.",
 "Get the apps to sync your files" => "Ekhavu la aplikaĵojn por sinkronigi viajn dosierojn",
+"Connect your desktop apps to %s" => "Konekti viajn labortablajn aplikaĵojn al %s",
 "Connect your Calendar" => "Konektu vian Kalendaron",
 "Connect your Contacts" => "Konektu viajn Kontaktojn",
 "Access files via WebDAV" => "Atingu dosierojn per WebDAV",
 "Documentation" => "Dokumentaro",
+"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Estas pli da informo en la <a target=\"_blank\" href=\"%s\">dokumentaro</a> kaj en nia <a target=\"_blank\" href=\"http://owncloud.org\">TTT-ejo</a>.",
 "If you like ownCloud, <a href=\"mailto:?subject=ownCloud&body=ownCloud is a great open software to sync and share your files. You can freely get it from http://owncloud.org\">recommend it to your friends</a>!" => "Se vi ŝatas ownCloud-on, <a href=\"mailto:?subject=ownCloud&body=ownCloud estas bonega malfermita programaro por sinkronigi kaj kunhavigi viajn dosierojn. Vi povas ekhavi ĝin senkoste el http://owncloud.org\">rekomendu ĝin al viaj amikoj</a>!"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
