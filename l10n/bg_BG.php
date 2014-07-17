1dcc9eb355713c4c279b72abd031fec863273e58
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-17 00:24:29 -0400

    [tx-robot] updated from transifex

--- l10n/bg_BG.php
+++ l10n/bg_BG.php
@@ -1,5 +1,6 @@
 <?php
 $TRANSLATIONS = array(
+"Get the apps to sync your files" => "Разреши приложенията да синхронизират файловете ти.",
 "Documentation" => "Документация"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ l10n/bn_IN.php
@@ -0,0 +1,5 @@
+<?php
+$TRANSLATIONS = array(
+"Documentation" => "নথিপত্র"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
