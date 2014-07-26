5aa4254a3b334a4e90ac281fc4811895fa021745
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-26 00:24:30 -0400

    [tx-robot] updated from transifex

--- l10n/fr.php
+++ l10n/fr.php
@@ -1,14 +1,14 @@
 <?php
 $TRANSLATIONS = array(
 "Welcome to %s" => "Bienvenue su %s",
-"Your personal web services. All your files, contacts, calendar and more, in one place." => "Vos services web personnels. Tous vos fichiers, vos contacts, agendas et bien plus encore, réunis en un seul endroit.",
-"Get the apps to sync your files" => "Obtenez les applications de synchronisation de vos fichiers",
+"Your personal web services. All your files, contacts, calendar and more, in one place." => "Vos services web personnels. Tous vos fichiers, vos contacts, agendas et bien plus encore, sont réunis en un seul endroit.",
+"Get the apps to sync your files" => "Obtenez les applications vous permettant de synchroniser vos fichiers",
 "Connect your desktop apps to %s" => "Connectez vos applications de bureau à %s",
 "Connect your Calendar" => "Connectez votre calendrier",
 "Connect your Contacts" => "Connectez vos contacts",
 "Access files via WebDAV" => "Accéder aux fichiers via WebDAV",
 "Documentation" => "Documentation",
 "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Des informations complémentaires sont disponibles dans la <a target=\"_blank\" href=\"%s\">documentation</a> et sur notre <a target=\"_blank\" href=\"http://owncloud.org\">site web</a>.",
-"If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files. \n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" => "Si vous aimez ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud est un super logiciel libre pour synchroniser et partager vos fichiers. \n\t\tVous pouvez gratuitement l'obtenir sur http://owncloud.org\">\n\t\trecommandez le à vos amis</a>\n\tet <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">faites passer le mot</a> !"
+"If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files. \n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" => "Si vous aimez ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud est un super logiciel libre pour synchroniser et partager vos fichiers. \n\t\tVous pouvez l'obtenir gratuitement sur http://owncloud.org\">\n\t\trecommandez le à vos amis</a>\n\tet <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">faites passer le mot</a> !"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
