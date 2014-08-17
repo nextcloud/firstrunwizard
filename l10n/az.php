85e231cfbdd5f0b82c502513defb51090e934632
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-17 00:24:27 -0400

    [tx-robot] updated from transifex

---
+++ l10n/az.php
@@ -0,0 +1,10 @@
+<?php
+$TRANSLATIONS = array(
+"Welcome to %s" => "%s -ə xoş gəlmisiniz",
+"Your personal web services. All your files, contacts, calendar and more, in one place." => "Sizin şəxsi web servisləriniz. Bir nöqtədə bütün sizin fayllar, əlaqələr, kalendar və. s.",
+"Get the apps to sync your files" => "Fayllarınızın sinxronizasiyası üçün proqramları götürün",
+"Connect your desktop apps to %s" => "Desktop proqramınızı %s-ə qoşun",
+"Connect your Calendar" => "Öz kalendarınıza qoşulun",
+"Documentation" => "Sənədlər"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
