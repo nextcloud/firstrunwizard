8ed7b17ea06037835eebb62f5385f4624ad13a71
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-10-30 06:57:20 -0400

    [tx-robot] updated from transifex

--- l10n/id.php
+++ l10n/id.php
@@ -1,14 +1,14 @@
 <?php
 $TRANSLATIONS = array(
 "Welcome to %s" => "Selamat datang di %s",
-"Your personal web services. All your files, contacts, calendar and more, in one place." => "Layanan web personal. Semua berkas, kontak, kalender dan hal lainnya milik  Anda, berada dalam satu tempat.",
-"Get the apps to sync your files" => "Dapatkan aplikasi untuk sinkronisasi berkas anda",
+"Your personal web services. All your files, contacts, calendar and more, in one place." => "Layanan web personal Anda. Semua berkas, kontak, kalender dan hal lainnya milik  Anda, berada dalam satu tempat.",
+"Get the apps to sync your files" => "Dapatkan aplikasi untuk sinkronisasi berkas Anda",
 "Connect your desktop apps to %s" => "Hubungkan aplikasi desktop Anda ke %s",
-"Connect your Calendar" => "Hubungkan Kalender anda",
-"Connect your Contacts" => "Hubungkan Kontak anda",
+"Connect your Calendar" => "Hubungkan Kalender Anda",
+"Connect your Contacts" => "Hubungkan Kontak Anda",
 "Access files via WebDAV" => "Akses berkas via WebDAV",
-"Documentation" => "dokumentasi",
-"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Terdapat informasi lebih lanjut di <a target=\"_blank\" href=\"%s\">dokumentasi</a> dan di <a target=\"_blank\" href=\"http://owncloud.org\">situs web</a> kami.",
+"Documentation" => "Dokumentasi",
+"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Tersedia informasi lebih lanjut di <a target=\"_blank\" href=\"%s\">dokumentasi</a> dan di <a target=\"_blank\" href=\"http://owncloud.org\">situs web</a> kami.",
 "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files. \n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" => "Jika Anda menyukai ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud adalah aplikasi terbuka hebat untuk menyinkronkan dan membagikan berkas-berkas Anda. \n\t\tAnda bisa mendapatkannya secara gratis dari http://owncloud.org\">\n\t\trekomndasikan ini kepada teman Anda</a>\n\tdan <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">sebarkan beritanya</a>!"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
