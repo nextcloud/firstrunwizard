30dbe7d9246bcdcf316d903adcb45b067d22ac9c
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-12-18 16:12:05 +0100

    accessibility fixes for first run wizard

--- templates/wizard.php
+++ templates/wizard.php
@@ -13,25 +13,34 @@
 
 <h2><?php p($l->t('Get the apps to sync your files'));?></h2>
 <a target="_blank" href="<?php p($_['clients']['desktop']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'desktopapp.png')); ?>" />
+	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'desktopapp.png')); ?>"
+		alt="<?php p($l->t('Desktop client'));?>" />
 </a>
 <a target="_blank" href="<?php p($_['clients']['android']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'googleplay.png')); ?>" />
+	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'googleplay.png')); ?>"
+		alt="<?php p($l->t('Android app'));?>" />
 </a>
 <a target="_blank" href="<?php p($_['clients']['ios']); ?>">
-	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'appstore.png')); ?>" />
+	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'appstore.png')); ?>"
+		alt="<?php p($l->t('iOS app'));?>" />
 </a>
 
 <?php if (OC_Util::getEditionString() === ''): ?>
 <h2><?php p($l->t('Connect your desktop apps to %s', array($theme->getName()))); ?></h2>
 <a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-calendars')) ?>">
-	<img class="appsmall appsmall-calendar svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/calendar-dark.svg')); ?>" /> <?php p($l->t('Connect your Calendar'));?>
+	<img class="appsmall appsmall-calendar svg" alt=""
+		src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/calendar-dark.svg')); ?>" />
+	<?php p($l->t('Connect your Calendar'));?>
 </a>
 <a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-contacts')) ?>">
-	<img class="appsmall appsmall-contacts svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/contacts-dark.svg')); ?>" /> <?php p($l->t('Connect your Contacts'));?>
+	<img class="appsmall appsmall-contacts svg" alt=""
+		src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/contacts-dark.svg')); ?>" />
+	<?php p($l->t('Connect your Contacts'));?>
 </a>
 <a target="_blank" class="button" href="<?php p(link_to_docs('user-webdav')); ?>">
-	<img class="appsmall svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/folder.svg')); ?>" /> <?php p($l->t('Access files via WebDAV'));?>
+	<img class="appsmall svg" alt=""
+		src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/folder.svg')); ?>" />
+	<?php p($l->t('Access files via WebDAV'));?>
 </a>
 <?php else: ?>
 <br><br><br>
