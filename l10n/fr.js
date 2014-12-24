7c5701494359f3e2ed8a159adc526eda535c9b8d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-12-24 00:24:12 -0500

    [tx-robot] updated from transifex

--- l10n/fr.js
+++ l10n/fr.js
@@ -4,6 +4,8 @@ OC.L10N.register(
     "Welcome to %s" : "Bienvenue sur %s",
     "Your personal web services. All your files, contacts, calendar and more, in one place." : "Vos services web personnels. Tous vos fichiers, contacts, agendas et bien plus encore, sont réunis en un seul endroit.",
     "Get the apps to sync your files" : "Obtenez les applications vous permettant de synchroniser vos fichiers",
+    "Android app" : "Application Android",
+    "iOS app" : "Application iOS",
     "Connect your desktop apps to %s" : "Connectez vos applications de bureau à %s",
     "Connect your Calendar" : "Connectez votre calendrier",
     "Connect your Contacts" : "Connectez vos contacts",
