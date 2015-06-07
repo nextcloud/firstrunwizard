25e9b119abcaf85cd032bac7a3b6690f97af686e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-07 00:24:31 -0400

    [tx-robot] updated from transifex

---
+++ l10n/th_TH.js
@@ -0,0 +1,18 @@
+OC.L10N.register(
+    "firstrunwizard",
+    {
+    "Welcome to %s" : "ยินดีต้อนรับสู่ %s",
+    "Your personal web services. All your files, contacts, calendar and more, in one place." : "บริการเว็บเซอร์วิสส่วนตัวของคุณ อาธิเช่น ไฟล์ทั้งหมดของคุณ, รายชื่อผู้ติดต่อ, ปฏิทินและอื่นๆ พร้อมทั้งความเสถียรและปลอดภัยบนโลกออนไลน์ของคุณ",
+    "Get the apps to sync your files" : "ใช้แอพพลิเคชันในการผสานไฟล์ของคุณ",
+    "Desktop client" : "เดสก์ทอปผู้ใช้",
+    "Android app" : "แอพฯ แอนดรอยด์",
+    "iOS app" : "แอพฯ IOS",
+    "Connect your desktop apps to %s" : "เชื่อมต่อแอพพลิเคชันเดสก์ทอปของคุณไปยัง %s",
+    "Connect your Calendar" : "เชื่อมต่อปฏิทินของคุณ",
+    "Connect your Contacts" : "เชื่อมต่อผู้ติดต่อของคุณ",
+    "Access files via WebDAV" : "เข้าถึงไฟล์ผ่าน WebDAV",
+    "Documentation" : "เอกสารคู่มือประกอบการใช้งาน",
+    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "มีข้อมูลเพิ่มเติมใน a target=\"_blank\" href=\"%s\">เอกสาร</a> และบน <a target=\"_blank\" href=\"http://owncloud.org\">เว็บไซต์</a> ของคุณ",
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files. \n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "ถ้าคุณชอบ ownCloud <a href=\"mailto:?subject=ownCloud&body=ownCloud เป็นโอเพนซอฟต์แวร์ที่ดีในการผสานข้อมูลและแชร์ไฟล์ของคุณ คุณได้รับมันจาก http://owncloud.org\">แนะนำให้เพื่อนของคุณสิ!</a> และ <a href=\"http://owncloud.org/promote\" target=\"_blank\">ใช้ดีช่วยบอกต่อ</a>!"
+},
+"nplurals=1; plural=0;");
