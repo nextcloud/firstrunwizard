dd45e47e58166474bebc6ec463a68aaf090fc9b0
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-08-19 00:24:22 -0400

    [tx-robot] updated from transifex

--- l10n/ja.js
+++ l10n/ja.js
@@ -3,7 +3,7 @@ OC.L10N.register(
     {
     "Welcome to %s" : "%s へようこそ",
     "Your personal web services. All your files, contacts, calendar and more, in one place." : "あなたの個人Webサービスです。すべてのファイル、連絡先、カレンダーなどを一つの場所で管理できます。",
-    "Get the apps to sync your files" : "ファイルを同期するためのアプリを取得",
+    "Get the apps to sync your files" : "ファイルを同期するアプリを取得しましょう",
     "Desktop client" : "デスクトップクライアント",
     "Android app" : "Androidアプリ",
     "iOS app" : "iOSアプリ",
@@ -13,6 +13,6 @@ OC.L10N.register(
     "Access files via WebDAV" : "WebDAV経由でファイルにアクセス",
     "Documentation" : "ドキュメント",
     "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "詳細な情報は、<a target=\"_blank\" href=\"%s\">ドキュメント</a> と <a target=\"_blank\" href=\"http://owncloud.org\">ウェブサイト</a> にあります。",
-    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files. \n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "もしownCloudを気に入ってもらえた場合は、\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud はファイル共有と同期のための素晴らしいオープンソース・ソフトウェアです。\n\t\thttp://owncloud.org から自由にダウンロードしてご利用いただけます\">\n\t\tぜひ友人に推薦をお願いします</a>。\n\t<a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">広く伝えてください</a>！"
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files. \n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "ownCloudを気に入っていただけたら、\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud はファイル共有と同期のための素晴らしいオープンソース・ソフトウェアです。\n\t\thttp://owncloud.org から自由にダウンロードしてご利用いただけます\">\n\t\tぜひ友人に推薦をお願いします</a>。\n\t<a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">広く伝えてください</a>！"
 },
 "nplurals=1; plural=0;");
