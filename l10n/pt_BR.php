9a5f985fcbcd5b640ec498d1837d96f0d097142a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-12-17 00:24:22 -0500

    [tx-robot] updated from transifex

--- l10n/pt_BR.php
+++ l10n/pt_BR.php
@@ -2,7 +2,7 @@
 $TRANSLATIONS = array(
 "A safe home for all your data" => "Um lugar seguro para todos os seus dados",
 "Access & share your files, calendars, contacts, mail & more from any device, on your terms" => "Acesse & compartilhe seus arquivos, agenda, contatos, email & tudo mais de qualquer dispositivo, em seus termos",
-"Get the apps to sync your files" => "Faça com que os apps sincronize seus arquivos",
+"Get the apps to sync your files" => "Obtenha apps para sincronizar seus arquivos",
 "Desktop client" => "Cliente Desktop",
 "Android app" => "App Android",
 "iOS app" => "App iOS",
@@ -11,7 +11,7 @@ $TRANSLATIONS = array(
 "Connect your Contacts" => "Conecte seus Contatos",
 "Documentation" => "Documentação",
 "Access files via WebDAV" => "Acesse arquivos via WebDAV",
-"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Existe mais informação em <a target=\"_blank\" href=\"%s\">documentação</a> e em nosso <a target=\"_blank\" href=\"http://owncloud.org\">website</a>",
+"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Existem mais informações em nossa <a target=\"_blank\" href=\"%s\">documentação</a> e em nosso <a target=\"_blank\" href=\"http://owncloud.org\">website</a>.",
 "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" => "Se você gosta do ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud é um ótimo software aberto para sincronizar e compartilhar seus arquivos.\n\t\tVocê pode obtê-lo livremente a partir de http://owncloud.org\">\n\t\trecomende a seus amigos</a>\n\te <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">espalhe pelo mundo</a>!"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
