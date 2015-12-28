d1b83f37fa8efb186169488802cd75d6c20f0055
Lukas Reschke <lukas@owncloud.com>
2015-12-28 19:23:14 +0100

    Use current year from server
    Saves us from updating this each year manually.

--- templates/wizard.php
+++ templates/wizard.php
@@ -59,7 +59,7 @@
 	and <a href="http://owncloud.org/promote"
 		target="_blank">spread the word</a>!')); ?>
 <?php else: ?>
-© 2011-2015 <a href="https://owncloud.com" target="_blank">ownCloud Inc.</a>
+© 2011-<?php p(date('Y')) ?> <a href="https://owncloud.com" target="_blank">ownCloud Inc.</a>
 <?php endif; ?>
 </p>
 
