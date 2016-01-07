080063657b0b4ea709ea55797f5d309494ea988f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-01-07 00:24:24 -0500

    [tx-robot] updated from transifex

--- l10n/he.php
+++ l10n/he.php
@@ -1,10 +1,15 @@
 <?php
 $TRANSLATIONS = array(
-"Your personal web services. All your files, contacts, calendar and more, in one place." => "שירותי הרשת האישיים שלך. כל הקבצים, אנשי הקשר ולוחות השנה שלך במקום אחד.",
+"A safe home for all your data" => "בית בטוח לכל המידע שלך",
+"Access & share your files, calendars, contacts, mail & more from any device, on your terms" => "כניסה ושיתוף של קבצים, לוחות השנה, פרטי קשר, דואר אלקטרוני ועוד מכל מכשיר, ובתנאים שלך",
 "Get the apps to sync your files" => "השג את האפליקציות על מנת לסנכרן את הקבצים שלך",
+"Desktop client" => "מחשב אישי",
+"Android app" => "אפליקציית אנרואיד",
+"iOS app" => "אפליקציית אייפון",
+"Connect your desktop apps to %s" => "חיבור אפליקציית המחשב האישי אל %s",
 "Connect your Calendar" => "חבר את לוח השנה שלך",
 "Connect your Contacts" => "חבר את אנשי הקשר שלך",
-"Access files via WebDAV" => "גש לקבצים דרך WebDAV",
-"Documentation" => "תיעוד"
+"Documentation" => "תיעוד",
+"Access files via WebDAV" => "גש לקבצים דרך WebDAV"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
