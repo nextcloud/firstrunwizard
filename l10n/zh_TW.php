1ed888ed0dc6acda8ac2d9fbd02f4e3c5d501be4
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-01-15 00:24:18 -0500

    [tx-robot] updated from transifex

--- l10n/zh_TW.php
+++ l10n/zh_TW.php
@@ -11,6 +11,7 @@ $TRANSLATIONS = array(
 "Connect your Contacts" => "連接您的聯絡人",
 "Documentation" => "說明文件",
 "Access files via WebDAV" => "透過 WebDAV 存取檔案",
-"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "可以從我們的<a target=\"_blank\" href=\"%s\">說明文件</a>和<a target=\"_blank\" href=\"http://owncloud.org\">網站</a>取得更多資訊"
+"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "可以從我們的<a target=\"_blank\" href=\"%s\">說明文件</a>和<a target=\"_blank\" href=\"http://owncloud.org\">網站</a>取得更多資訊",
+"If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" => "如果你喜歡 ownCloud，\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud 是一套可以同步和分享檔案的超棒自由軟體， \n\t\t你可以在 http://owncloud.org 免費取得\">\n\t\t將它推薦給朋友！</a>\n\t然後<a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">廣為宣傳</a>！"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
