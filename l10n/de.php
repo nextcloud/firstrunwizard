15cd8e5a9514152e0fd8aec55ae22b48544b3206
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-02-24 15:15:23 -0500

    [tx-robot] updated from transifex

--- l10n/de.php
+++ l10n/de.php
@@ -11,6 +11,7 @@ $TRANSLATIONS = array(
 "Connect your Contacts" => "Verbinde Deine Kontakte",
 "Documentation" => "Dokumentation",
 "Access files via WebDAV" => "Greife auf Dateien über WebDAV zu",
-"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Weitere Informationen findest Du in der <a target=\"_blank\" href=\"%s\">Dokumentation</a> und auf unserer <a target=\"_blank\" href=\"http://owncloud.org\">Webseite</a>."
+"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Weitere Informationen findest Du in der <a target=\"_blank\" href=\"%s\">Dokumentation</a> und auf unserer <a target=\"_blank\" href=\"http://owncloud.org\">Webseite</a>.",
+"If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" => "Wenn sie ownCloud mögen,\n\n⇥<a href=\"mailto:?subject=ownCloud\n\n⇥⇥&body=ownCloud ist eine offene Software mit der du deine Daten synchronisieren und teilen kannst.\n\n⇥⇥Du kannst es dir hier runterladen http://owncloud.org\">\n\n⇥⇥empfehle es deinen Freunden</a>\n\n⇥und <a href=\"http://owncloud.org/promote\"\n\n⇥⇥target=\"_blank\">gib es an andere weiter</a>!"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
--- l10n/en_GB.php
+++ l10n/en_GB.php
@@ -1,5 +1,7 @@
 <?php
 $TRANSLATIONS = array(
+"A safe home for all your data" => "A safe home for all your data",
+"Access & share your files, calendars, contacts, mail & more from any device, on your terms" => "Access & share your files, calendars, contacts, mail & more from any device, on your terms",
 "Get the apps to sync your files" => "Get the apps to sync your files",
 "Desktop client" => "Desktop client",
 "Android app" => "Android app",
@@ -9,6 +11,7 @@ $TRANSLATIONS = array(
 "Connect your Contacts" => "Connect your Contacts",
 "Documentation" => "Documentation",
 "Access files via WebDAV" => "Access files via WebDAV",
-"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>."
+"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>.",
+"If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" => "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
