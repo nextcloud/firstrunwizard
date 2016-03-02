275e692ccc20609283bc02f962458f8658a328d0
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-02 00:24:30 -0500

    [tx-robot] updated from transifex

---
+++ l10n/is.js
@@ -0,0 +1,18 @@
+OC.L10N.register(
+    "firstrunwizard",
+    {
+    "A safe home for all your data" : "Öruggur stður fyrir öll gögnin þín",
+    "Access & share your files, calendars, contacts, mail & more from any device, on your terms" : "Fáðu aðgang að og deildu skrám, dagatölum, tengiliðum, tölvupósti og ýmsu fleiru, á hvaða tæki sem er og á þínum eigin forsendum",
+    "Get the apps to sync your files" : "Náðu í forrit til að samstilla skrárnar þínar",
+    "Desktop client" : "Skjáborðsforrit",
+    "Android app" : "Android-forrit",
+    "iOS app" : "iOS-forrit",
+    "Connect your desktop apps to %s" : "Tengdu skjáborðsforritin við %s",
+    "Connect your Calendar" : "Tengdu dagatalið þitt",
+    "Connect your Contacts" : "Tengdu tengiliðalistana þína",
+    "Documentation" : "Hjálparskjöl",
+    "Access files via WebDAV" : "Nálgastu skrár í gegnum WebDAV",
+    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Til að fá meiri upplýsingar má skoða <a target=\"_blank\" href=\"%s\">hjálparskjölin</a> og <a target=\"_blank\" href=\"http://owncloud.org\">vefsvæðið okkar</a>.",
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "Ef þér líkar ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\tskaltu mæla með því við vini þína</a>\n\tog <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">láttu orð út ganga</a>!"
+},
+"nplurals=2; plural=(n % 10 != 1 || n % 100 == 11);");
