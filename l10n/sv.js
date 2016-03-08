c7669c8fe30bbc2eab71a9be3124a62c6c5dd318
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-08 00:24:26 -0500

    [tx-robot] updated from transifex

--- l10n/sv.js
+++ l10n/sv.js
@@ -1,8 +1,8 @@
 OC.L10N.register(
     "firstrunwizard",
     {
-    "A safe home for all your data" : "Ett säkert ställe för all din data",
-    "Access & share your files, calendars, contacts, mail & more from any device, on your terms" : "Din personliga webbtjänst. Synka & dela dina filer, din kalender, kontakter, mail & ännu mer från vilken enhet som helst, på dina villkor",
+    "A safe home for all your data" : "En säker lagringsplats för all din data",
+    "Access & share your files, calendars, contacts, mail & more from any device, on your terms" : "Din personliga webbtjänst. Synka & dela dina filer, kalender, kontakter, mail och ännu mer från vilken enhet som helst, på dina villkor",
     "Get the apps to sync your files" : "Skaffa appar för att synkronisera dina filer",
     "Desktop client" : "Skrivbordsklient",
     "Android app" : "Android app",
@@ -13,6 +13,6 @@ OC.L10N.register(
     "Documentation" : "Dokumentation",
     "Access files via WebDAV" : "Åtkomst av filer via WebDAV",
     "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Det finns mer information i <a target=\"_blank\" href=\"%s\">dokumentationen</a> och på vår <a target=\"_bland\" href=\"http://owncloud.org\">hemsida</a>",
-    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "Om du gillar ownCloud.\n⇥<a href=\"mailto:?subject=ownCloud\n⇥⇥&body=ownCloud är en fantastisk mjukvara för att synka och dela dina filer.\n⇥⇥You can freely get it from http://owncloud.org\">\n⇥⇥rekommendera det till dina vänner</a>\n⇥och <a href=\"http://owncloud.org/promote\"\n⇥⇥target=\"_blank\"sprid det vidare</a>!"
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "Om du gillar ownCloud, <a href=\"mailto:?subject=ownCloud&body=ownCloud är en riktigt bra open-source mjukvara för att synka och dela dina filer. Du kan hämta det gratis från http://owncloud.org\"> rekomendera det till dina vänner</a> och <a href=\"http://owncloud.org/promote\" target=\"_blank\">sprid det vidare</a>!"
 },
 "nplurals=2; plural=(n != 1);");
