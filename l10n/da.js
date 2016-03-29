93263361e2ac46d6c488b55236b8cbd475b8d748
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-03-29 00:24:24 -0400

    [tx-robot] updated from transifex

--- l10n/da.js
+++ l10n/da.js
@@ -12,6 +12,7 @@ OC.L10N.register(
     "Connect your Contacts" : "Tilslut dine kontaktpersoner",
     "Documentation" : "Dokumentation",
     "Access files via WebDAV" : "Adgang til filer via WebDAV",
-    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Der er mere information i <a target=\"_blank\" href=\"%s\">dokumentationen</a> og på vores <a target=\"_blank\" href=\"http://owncloud.org\">hjemmeside</a>."
+    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Der er mere information i <a target=\"_blank\" href=\"%s\">dokumentationen</a> og på vores <a target=\"_blank\" href=\"http://owncloud.org\">hjemmeside</a>.",
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "Hvis du sætter pris på ownCloud, så <a href=\"mailto:?subject=ownCloud&body=ownCloud er en fantastisk og åben softwareløsning til synkronisering og deling af dine filer. Du kan hente den frit på http://owncloud.org\">anbefal den til dine venner</a> og <a href=\"http://owncloud.org/promote\" target=\"_blank\">spred budskabet</a>!"
 },
 "nplurals=2; plural=(n != 1);");
