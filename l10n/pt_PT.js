088502f079bda8138585e8e9735151d5f133656a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-06-03 00:30:43 -0400

    [tx-robot] updated from transifex

--- l10n/pt_PT.js
+++ l10n/pt_PT.js
@@ -2,17 +2,17 @@ OC.L10N.register(
     "firstrunwizard",
     {
     "A safe home for all your data" : "Um local seguro para todos os seus dados",
-    "Access & share your files, calendars, contacts, mail & more from any device, on your terms" : "Aceda e partilhe os seus ficheiros, calendários, contatos, correio e muito mais a partir de qualquer aparelho, nos seus termos",
+    "Access & share your files, calendars, contacts, mail & more from any device, on your terms" : "Aceda e compartilhe os seus ficheiros, calendários, contactos, correio e muito mais a partir de qualquer dispositivo, nos seus termos",
     "Get the apps to sync your files" : "Obtenha as aplicações para sincronizar os seus ficheiros",
-    "Desktop client" : "Cliente Desktop",
+    "Desktop client" : "Cliente de PC",
     "Android app" : "Aplicação Android",
     "iOS app" : "Aplicação iOS",
-    "Connect your desktop apps to %s" : "Ligue as suas aplicações do Ambiente de Trabalho a %s",
+    "Connect your desktop apps to %s" : "Ligue as suas aplicações de PC para %s",
     "Connect your Calendar" : "Ligar o seu calendário",
     "Connect your Contacts" : "Ligar os seus contactos",
     "Documentation" : "Documentação",
     "Access files via WebDAV" : "Aceder aos ficheiros via WebDAV",
-    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Encontre mais informação na <a target=\"_blank\" href=\"%s\">documentação</a> e no nosso <a target=\"_blank\" href=\"http://owncloud.org\">sítio</a>.",
-    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "Se gosta do ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud é um software aberto excelente para sincronizar e partilhar os seus ficheiros.\n\t\tPode obtê-lo aqui gratuitamente http://owncloud.org\">\n\t\trecomende aos seus amigos</a>\n\te <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">espalhe a palavra</a>!"
+    "There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." : "Existe mais informação na <a target=\"_blank\" href=\"%s\">documentação</a> e no nosso <a target=\"_blank\" href=\"http://owncloud.org\">site da Web</a>.",
+    "If you like ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud is a great open software to sync and share your files.\n\t\tYou can freely get it from http://owncloud.org\">\n\t\trecommend it to your friends</a>\n\tand <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">spread the word</a>!" : "Se gosta do ownCloud,\n\t<a href=\"mailto:?subject=ownCloud\n\t\t&body=ownCloud é um software aberto excelente para sincronizar e compartilhar os seus ficheiros.\n\t\tPode obtê-lo aqui gratuitamente http://owncloud.org\">\n\t\trecomende aos seus amigos</a>\n\te <a href=\"http://owncloud.org/promote\"\n\t\ttarget=\"_blank\">espalhe a palavra</a>!"
 },
 "nplurals=2; plural=(n != 1);");
