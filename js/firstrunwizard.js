e7fc5d97efc0c4c3a264dc9a40167d989d90b9b1
Julius Härtl <jus@bitgrid.net>
2018-04-06 14:24:57 +0200

    Add multiple steps to the first run wizard
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- js/firstrunwizard.js
+++ js/firstrunwizard.js
@@ -1,3 +1,19 @@
+function getTotal() {
+	return $('#firstrunwizard .page').length;
+}
+
+function getCurrent() {
+	return $('#firstrunwizard .page.active').index('#firstrunwizard .page');
+}
+
+function isLast() {
+	return getCurrent() >= getTotal()-1;
+}
+
+function isFirst() {
+	return getCurrent() === 0;
+}
+
 function showFirstRunWizard(){
 	$.colorbox({
 		opacity: 0.7,
@@ -11,7 +27,59 @@ function showFirstRunWizard(){
 				url: OC.generateUrl('/apps/firstrunwizard/wizard'),
 				type: 'delete'
 			});
-		}
+		},
+		onComplete: function() {
+			var $wizard = $('#firstrunwizard');
+			var $finish = $wizard.find('#finish');
+			var $prev = $wizard.find('#prev');
+			var $next = $wizard.find('#next');
+			var $pages = $wizard.find('.page');
+			$finish.hide();
+			$pages.hide();
+			$prev.hide();
+			$pages.first().fadeIn().addClass('active');
+			$.colorbox.resize();
+			$wizard.on('click', '#next', function() {
+				var active = $('#firstrunwizard .active');
+				var next = active.next();
+				$prev.show();
+				if (getCurrent() < getTotal()-1) {
+					active.hide().removeClass('active');
+					next.show().addClass('active');
+				}
+
+				if (isLast()) {
+					$next.hide();
+					$finish.show();
+				} else {
+					$next.show();
+					$finish.hide();
+				}
+				$.colorbox.resize();
+			});
+			$('#firstrunwizard').on('click', '#prev', function() {
+				var active = $('#firstrunwizard .active');
+				var prev = active.prev();
+				active.hide().removeClass('active');
+				prev.show().addClass('active');
+				$next.show();
+				$finish.hide();
+				if (!isFirst()) {
+					active.hide().removeClass('active');
+					prev.show().addClass('active');
+				}
+
+				if (isFirst()) {
+					$prev.hide();
+				} else {
+					$prev.show();
+				}
+				$.colorbox.resize();
+			});
+			$('#firstrunwizard').on('click', '#finish', function() {
+				$.colorbox.close();
+			});
+		},
 	});
 }
 
