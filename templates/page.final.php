e8b43eb9197bffb8bc0abbaa53879a34ba857f42
Julius Härtl <jus@bitgrid.net>
2019-01-07 20:04:26 +0100

    Use target="_blank" and rel="noreferrer noopener"
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- templates/page.final.php
+++ templates/page.final.php
@@ -12,14 +12,14 @@
 			<h3 class="icon-info"><?php p($l->t('Get more information')); ?></h3>
 			<p><?php p($l->t('The Nextcloud documentation for home users:')); ?></p>
 			<ul>
-				<li><a href="<?php p(link_to_docs('user-')) ?>"><?php p($l->t('User manual')); ?></a></li>
-				<li><a href="<?php p(link_to_docs('admin-')) ?>"><?php p($l->t('Admin manual')); ?></a></li>
-				<li><a href="<?php p(link_to_docs('developer-')) ?>"><?php p($l->t('Developer manual')); ?></a></li>
+				<li><a href="<?php p(link_to_docs('user-')) ?>" target="_blank" rel="noreferrer noopener"><?php p($l->t('User manual')); ?></a></li>
+				<li><a href="<?php p(link_to_docs('admin-')) ?>" target="_blank" rel="noreferrer noopener"><?php p($l->t('Admin manual')); ?></a></li>
+				<li><a href="<?php p(link_to_docs('developer-')) ?>" target="_blank" rel="noreferrer noopener"><?php p($l->t('Developer manual')); ?></a></li>
 			</ul>
 			<p><?php p($l->t('You can also ask for help in our community support channels:')); ?></p>
 			<ul>
-				<li><a href="https://help.nextcloud.com"><?php p($l->t('the Nextcloud forums')); ?></a></li>
-				<li><a href="https://riot.im/app/#/room/#freenode_#nextcloud:matrix.org"><?php p($l->t('the Nextcloud IRC chat channel on freenode.net')); ?></a></li>
+				<li><a href="https://help.nextcloud.com" target="_blank" rel="noreferrer noopener"><?php p($l->t('the Nextcloud forums')); ?></a></li>
+				<li><a href="https://riot.im/app/#/room/#freenode_#nextcloud:matrix.org" target="_blank" rel="noreferrer noopener"><?php p($l->t('the Nextcloud IRC chat channel on freenode.net')); ?></a></li>
 			</ul>
 		</div>
 	</div>
@@ -27,12 +27,12 @@
 		<div class="description-block">
 			<h3 class="icon-user"><?php p($l->t('Start contributing')); ?></h3>
 			<p><?php p($l->t('Do you want to get a certain improvement in Nextcloud? Did you find a problem? Do you want to help translate, promote or document Nextcloud?')); ?></p>
-			<a href="https://nextcloud.com/contribute/" class="button"><?php p($l->t('Become part of the Community')); ?></a>
+			<a href="https://nextcloud.com/contribute/" class="button" target="_blank" rel="noreferrer noopener"><?php p($l->t('Become part of the Community')); ?></a>
 		</div>
 		<div class="description-block">
 			<h3 class="icon-world"><?php p($l->t('Enterprise support')); ?></h3>
 			<p><?php p($l->t('If you run Nextcloud in a mission critical environment with large numbers of users and big amounts of data and need the certainty of support from the experts behind the Nextcloud technology, an Enterprise Subscription from Nextcloud is available with email and phone support.')); ?></p>
-			<a href="https://nextcloud.com/enterprise/buy" class="button"><?php p($l->t('Get enterprise support')); ?></a>
+			<a href="https://nextcloud.com/enterprise/buy" class="button"target="_blank" rel="noreferrer noopener" ><?php p($l->t('Get enterprise support')); ?></a>
 		</div>
 	</div>
 </div>
