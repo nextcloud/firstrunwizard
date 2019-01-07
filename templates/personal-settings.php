bcc97194b028b331d6f5c46becd008b3171465de
Julius Härtl <jus@bitgrid.net>
2019-01-07 20:04:26 +0100

    Use target="_blank" and rel="noreferrer noopener"
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- templates/personal-settings.php
+++ templates/personal-settings.php
@@ -33,15 +33,15 @@ script('firstrunwizard', ['jquery.colorbox', 'firstrunwizard']);
 	<p><?php p($l->t('%s gives you access to your files wherever you are. Our easy to use desktop and mobile clients are available for all major platforms at zero cost.', [$theme->getName()])); ?></p>
 
 	<div class="clientslinks">
-		<a href="<?php p($_['clients']['desktop']); ?>" rel="noreferrer" target="_blank">
+		<a href="<?php p($_['clients']['desktop']); ?>" rel="noreferrer noopener" target="_blank">
 			<img src="<?php print_unescaped(image_path('core', 'desktopapp.svg')); ?>"
 				 alt="<?php p($l->t('Desktop client'));?>"/>
 		</a>
-		<a href="<?php p($_['clients']['android']); ?>" rel="noreferrer" target="_blank">
+		<a href="<?php p($_['clients']['android']); ?>" rel="noreferrer noopener" target="_blank">
 			<img src="<?php print_unescaped(image_path('core', 'googleplay.png')); ?>"
 				 alt="<?php p($l->t('Android app'));?>" />
 		</a>
-		<a href="<?php p($_['clients']['ios']); ?>" rel="noreferrer" target="_blank">
+		<a href="<?php p($_['clients']['ios']); ?>" rel="noreferrer noopener" target="_blank">
 			<img src="<?php print_unescaped(image_path('core', 'appstore.svg')); ?>"
 				 alt="<?php p($l->t('iOS app'));?>" />
 		</a>
@@ -57,17 +57,17 @@ script('firstrunwizard', ['jquery.colorbox', 'firstrunwizard']);
 	<p><?php print_unescaped($l->t('Besides the sync clients you can connect any other software that supports the WebDAV/CalDAV/CardDAV protocols to %s.', [$theme->getName()])); ?></p>
 
 	<div class="clientslinks">
-		<a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-calendars')) ?>">
+		<a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-calendars')) ?>" rel="noreferrer noopener">
 			<img class="appsmall appsmall-calendar svg" alt=""
 				 src="<?php p(image_path('core', 'places/calendar.svg')); ?>" />
 			<?php p($l->t('Connect your calendar'));?>
 		</a>
-		<a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-contacts')) ?>">
+		<a target="_blank" class="button" href="<?php p(link_to_docs('user-sync-contacts')) ?>" rel="noreferrer noopener">
 			<img class="appsmall appsmall-contacts svg" alt=""
 				 src="<?php p(image_path('core', 'places/contacts.svg')); ?>" />
 			<?php p($l->t('Connect your contacts'));?>
 		</a>
-		<a target="_blank" class="button" href="<?php p(link_to_docs('user-webdav')); ?>">
+		<a target="_blank" class="button" href="<?php p(link_to_docs('user-webdav')); ?>" rel="noreferrer noopener">
 			<img class="appsmall svg" alt=""
 				 src="<?php p(image_path('files', 'folder.svg')); ?>" />
 			<?php p($l->t('Access files via WebDAV'));?>
