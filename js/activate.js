236df0c3c10fced9483dc8093fde4c6bb9da8f92
Julius Härtl <jus@bitgrid.net>
2019-01-15 17:55:33 +0100

    Properly handle activation script
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- js/activate.js
+++ js/activate.js
@@ -1,15 +1,5 @@
-function ready(fn) {
-  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}
-
-ready(function() {
-	setTimeout(function() {
-		OCP.Loader.loadScript('firstrunwizard', 'firstrunwizard.js').then(function() {
-			OCA.FirstRunWizard.open();
-		});
-	}, 100);
-})
+document.addEventListener('DOMContentLoaded', function() {
+	window.OCP.Loader.loadScript('firstrunwizard', 'firstrunwizard.js').then(function() {
+		OCA.FirstRunWizard.open();
+	});
+});
