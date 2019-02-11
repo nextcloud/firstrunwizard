bbf6ec657737c24a3e2fc7248b80de81bbd0ac4b
Julius Härtl <jus@bitgrid.net>
2019-02-11 15:41:37 +0100

    Only add event listener if selector can be found
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- js/about.js
+++ js/about.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-	document.querySelector('#expanddiv li[data-id="firstrunwizard-about"] a').addEventListener('click', function (event) {
-		event.stopPropagation();
-		event.preventDefault();
-		OCP.Loader.loadScript('firstrunwizard', 'firstrunwizard.js').then(function() {
-			OCA.FirstRunWizard.open();
-			OC.hideMenus(function () {
-				return false;
+	var aboutEntry = document.querySelector('#expanddiv li[data-id="firstrunwizard-about"] a');
+	if (aboutEntry) {
+		aboutEntry.addEventListener('click', function (event) {
+			event.stopPropagation();
+			event.preventDefault();
+			OCP.Loader.loadScript('firstrunwizard', 'firstrunwizard.js').then(function () {
+				OCA.FirstRunWizard.open();
+				OC.hideMenus(function () {
+					return false;
+				});
 			});
+			return true;
 		});
-		return true;
-	});
+	}
 });
