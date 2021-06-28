6f13951bad39d027ec416d0a7542061ad4cbf2ea
Julius Härtl <jus@bitgrid.net>
2021-06-28 10:36:38 +0200

    Fix eslint and bump bundles
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/App.vue
+++ src/App.vue
@@ -37,6 +37,212 @@
 		</div>
 	</Modal>
 </template>
+<script>
+import Modal from '@nextcloud/vue/dist/Components/Modal'
+import axios from '@nextcloud/axios'
+import { generateUrl } from '@nextcloud/router'
+import IntroVideo from './components/IntroVideo'
+
+export default {
+	name: 'App',
+	components: {
+		Modal,
+	},
+	data() {
+		return {
+			showModal: false,
+			withIntro: true,
+			slides: [],
+			currentSlide: 0,
+			fadeDirection: 'next',
+			isMobile: window.outerWidth < 1024,
+			slidesLoaded: false,
+		}
+	},
+	computed: {
+		slideList() {
+			if (this.withIntro) {
+				return this.slides
+			}
+			const slides = this.slides
+			return slides.slice(1)
+		},
+		hasNext() {
+			return this.currentSlide < this.slideList.length - 1
+		},
+		hasPrevious() {
+			return this.currentSlide > 0
+		},
+		isLast() {
+			return this.currentSlide === this.slideList.length - 1
+		},
+		isFirst() {
+			return this.currentSlide === 0
+		},
+		startButtonText() {
+			return t('firstrunwizard', 'Start using {cloudName}', { cloudName: window.OC.theme.name })
+		},
+	},
+	async created() {
+		this.slides = [IntroVideo]
+		window.addEventListener('resize', this.onResize)
+	},
+	beforeDestroy() {
+		window.removeEventListener('resize', this.onResize)
+	},
+	methods: {
+		async loadStaticSlides() {
+			if (this.slidesLoaded) {
+				return
+			}
+
+			try {
+				const response = await axios.get(generateUrl('/apps/firstrunwizard/wizard'))
+				this.slides.push(...response.data.slides)
+				this.withIntro = response.data.hasVideo
+				this.slidesLoaded = true
+			} catch (e) {
+				console.error('Failed to load slides')
+			}
+		},
+		async open(withIntro = true) {
+			await this.loadStaticSlides()
+			this.withIntro = this.withIntro & withIntro
+			this.showModal = true
+			this.currentSlide = 0
+		},
+		close() {
+			this.showModal = false
+			axios.delete(generateUrl('/apps/firstrunwizard/wizard'))
+		},
+		next() {
+			this.fadeDirection = 'next'
+			if (this.isLast) {
+				this.close()
+				return
+			}
+			this.currentSlide += 1
+		},
+		previous() {
+			this.fadeDirection = 'previous'
+			if (this.isFirst) {
+				return
+			}
+			this.currentSlide -= 1
+		},
+		onResize(event) {
+			// Update mobile mode
+			this.isMobile = window.outerWidth < 768
+		},
+	},
+}
+</script>
+<style lang="scss" scoped>
+	.modal-mask {
+		background-color: rgba(0, 0, 0, 0.7);
+
+		&::v-deep .modal-wrapper {
+			position: relative;
+		}
+
+		&::v-deep .modal-container {
+			display: flex;
+			flex-direction: column;
+			height: 95% !important;
+			width: 95% !important;
+			max-width: 900px;
+			max-height: 650px !important;
+			position: relative;
+		}
+
+		.modal-body {
+			flex-grow: 1;
+			display: flex;
+			overflow-x: hidden;
+			overflow-y: auto;
+
+			& > div {
+				display: flex;
+				flex-grow: 1;
+				align-items: center;
+				justify-content: center;
+			}
+		}
+	}
+
+	.modal-header {
+		height: 180px;
+		max-height: 40vh;
+		overflow: hidden;
+		flex-shrink: 0;
+
+		.firstrunwizard-header {
+			padding: 20px 12px;
+			background: var(--color-primary) var(--image-login-background) no-repeat 50% 50%;
+			background-size: cover;
+			color: var(--color-primary-text);
+			text-align: center;
+			.logo {
+				background: var(--image-logo) no-repeat center;
+				background-size: contain;
+				width: 175px;
+				height: 100px;
+				max-height: 20vh;
+				margin: 0 auto;
+			}
+			h2 {
+				font-size: 20px;
+				margin-top: 7px;
+				line-height: 150%;
+				color: var(--color-primary-text);
+				font-weight: 300;
+				padding: 0 0 10px;
+			}
+		}
+	}
+
+	.modal-default-button {
+		align-self: flex-end;
+	}
+
+	.modal-footer {
+		overflow: hidden;
+		position: absolute;
+		display: flex;
+		bottom: 0;
+		right: 0;
+	}
+
+	.modal-footer button {
+		margin: 10px;
+	}
+
+	/* Transitions */
+	.next-enter-active, .next-leave-active,
+	.previous-enter-active, .previous-leave-active {
+		transition: transform .1s, opacity .25s;
+	}
+
+	.next-enter {
+		transform: translateX(50%);
+		opacity: 0;
+	}
+
+	.next-leave-to {
+		transform: translateX(-50%);
+		opacity: 0;
+	}
+
+	.previous-enter {
+		transform: translateX(-50%);
+		opacity: 0;
+	}
+
+	.previous-leave-to {
+		transform: translateX(50%);
+		opacity: 0;
+	}
+</style>
 <style lang="scss">
 	/* Page styling needs to be unscoped, since we load it separately from the server */
 	#firstrunwizard {
@@ -220,210 +426,3 @@
 		}
 	}
 </style>
-
-<style lang="scss" scoped>
-	.modal-mask {
-		background-color: rgba(0, 0, 0, 0.7);
-
-		&::v-deep .modal-wrapper {
-			position: relative;
-		}
-
-		&::v-deep .modal-container {
-			display: flex;
-			flex-direction: column;
-			height: 95% !important;
-			width: 95% !important;
-			max-width: 900px;
-			max-height: 650px !important;
-			position: relative;
-		}
-
-		.modal-body {
-			flex-grow: 1;
-			display: flex;
-			overflow-x: hidden;
-			overflow-y: auto;
-
-			& > div {
-				display: flex;
-				flex-grow: 1;
-				align-items: center;
-				justify-content: center;
-			}
-		}
-	}
-
-	.modal-header {
-		height: 180px;
-		max-height: 40vh;
-		overflow: hidden;
-		flex-shrink: 0;
-
-		.firstrunwizard-header {
-			padding: 20px 12px;
-			background: var(--color-primary) var(--image-login-background) no-repeat 50% 50%;
-			background-size: cover;
-			color: var(--color-primary-text);
-			text-align: center;
-			.logo {
-				background: var(--image-logo) no-repeat center;
-				background-size: contain;
-				width: 175px;
-				height: 100px;
-				max-height: 20vh;
-				margin: 0 auto;
-			}
-			h2 {
-				font-size: 20px;
-				margin-top: 7px;
-				line-height: 150%;
-				color: var(--color-primary-text);
-				font-weight: 300;
-				padding: 0 0 10px;
-			}
-		}
-	}
-
-	.modal-default-button {
-		align-self: flex-end;
-	}
-
-	.modal-footer {
-		overflow: hidden;
-		position: absolute;
-		display: flex;
-		bottom: 0;
-		right: 0;
-	}
-
-	.modal-footer button {
-		margin: 10px;
-	}
-
-	/* Transitions */
-	.next-enter-active, .next-leave-active,
-	.previous-enter-active, .previous-leave-active {
-		transition: transform .1s, opacity .25s;
-	}
-
-	.next-enter {
-		transform: translateX(50%);
-		opacity: 0;
-	}
-
-	.next-leave-to {
-		transform: translateX(-50%);
-		opacity: 0;
-	}
-
-	.previous-enter {
-		transform: translateX(-50%);
-		opacity: 0;
-	}
-
-	.previous-leave-to {
-		transform: translateX(50%);
-		opacity: 0;
-	}
-</style>
-<script>
-import Modal from '@nextcloud/vue/dist/Components/Modal'
-import axios from '@nextcloud/axios'
-import { generateUrl } from '@nextcloud/router'
-import IntroVideo from './components/IntroVideo'
-
-export default {
-	name: 'App',
-	components: {
-		Modal,
-	},
-	data() {
-		return {
-			showModal: false,
-			withIntro: true,
-			slides: [],
-			currentSlide: 0,
-			fadeDirection: 'next',
-			isMobile: window.outerWidth < 1024,
-			slidesLoaded: false,
-		}
-	},
-	computed: {
-		slideList() {
-			if (this.withIntro) {
-				return this.slides
-			}
-			const slides = this.slides
-			return slides.slice(1)
-		},
-		hasNext() {
-			return this.currentSlide < this.slideList.length - 1
-		},
-		hasPrevious() {
-			return this.currentSlide > 0
-		},
-		isLast() {
-			return this.currentSlide === this.slideList.length - 1
-		},
-		isFirst() {
-			return this.currentSlide === 0
-		},
-		startButtonText() {
-			return t('firstrunwizard', 'Start using {cloudName}', { cloudName: window.OC.theme.name })
-		},
-	},
-	async created() {
-		this.slides = [ IntroVideo ]
-		window.addEventListener('resize', this.onResize)
-	},
-	beforeDestroy() {
-		window.removeEventListener('resize', this.onResize)
-	},
-	methods: {
-		async loadStaticSlides() {
-			if (this.slidesLoaded) {
-				return
-			}
-
-			try {
-				const response = await axios.get(generateUrl('/apps/firstrunwizard/wizard'))
-				this.slides.push(...response.data.slides)
-				this.withIntro = response.data.hasVideo
-				this.slidesLoaded = true
-			} catch (e) {
-				console.error('Failed to load slides')
-			}
-		},
-		async open(withIntro = true) {
-			await this.loadStaticSlides()
-			this.withIntro = this.withIntro & withIntro
-			this.showModal = true
-			this.currentSlide = 0
-		},
-		close() {
-			this.showModal = false
-			axios.delete(generateUrl('/apps/firstrunwizard/wizard'))
-		},
-		next() {
-			this.fadeDirection = 'next'
-			if (this.isLast) {
-				this.close()
-				return
-			}
-			this.currentSlide += 1
-		},
-		previous() {
-			this.fadeDirection = 'previous'
-			if (this.isFirst) {
-				return
-			}
-			this.currentSlide -= 1
-		},
-		onResize(event) {
-			// Update mobile mode
-			this.isMobile = window.outerWidth < 768
-		},
-	},
-}
-</script>
