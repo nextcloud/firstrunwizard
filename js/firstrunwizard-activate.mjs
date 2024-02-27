d4a55098d617e4b65d3d9ebc349f604467232ba1
Ferdinand Thiessen <opensource@fthiessen.de>
2024-02-27 14:08:40 +0100

    chore: Compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ js/firstrunwizard-activate.mjs
@@ -0,0 +1,8 @@
+/*! third party licenses: js/vendor.LICENSE.txt */
+import{_ as t}from"./modulepreload-polyfill-hfIVPvwd.mjs";document.addEventListener("DOMContentLoaded",async function(){(await t(()=>import("./main-BwB0SHeG.mjs"),__vite__mapDeps([0,1]),import.meta.url)).open()});
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = [OC.filePath('firstrunwizard', '', 'js/main-BwB0SHeG.mjs'),OC.filePath('firstrunwizard', '', 'css/firstrunwizard-main.css')]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
