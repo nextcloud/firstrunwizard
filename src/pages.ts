8647d55818f2605c9ceba0acdadcbfa25d52b80b
Ferdinand Thiessen <opensource@fthiessen.de>
2024-03-27 14:43:10 +0100

    feat: Implemented Nextcloud Hub changelog
    This adds additional slides to the wizard for changes in the current Nextcloud Hub release
    and for sharing it with other users.
    Also modified the menu entry from "About" to "About & What's new".
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/pages.ts
+++ src/pages.ts
@@ -1,10 +1,37 @@
+/**
+ * @copyright Copyright (c) 2024 Ferdinand Thiessen <opensource@fthiessen.de>
+ *
+ * @author Ferdinand Thiessen <opensource@fthiessen.de>
+ *
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import type { Component } from 'vue'
 
 import { translate as t } from '@nextcloud/l10n'
 
-import AboutNextcloud from './components/pages/AboutNextcloud.vue'
-import DeviceIntegration from './components/pages/DeviceIntegration.vue'
-import KeyNotes from './components/pages/KeyNotes.vue'
+import AboutNextcloudPage from './components/pages/AboutNextcloud.vue'
+import DeviceIntegrationPage from './components/pages/DeviceIntegration.vue'
+import HubReleasePage from './components/pages/HubRelease.vue'
+import KeyNotesPage from './components/pages/KeyNotes.vue'
+import SharePage from './components/pages/SharePage.vue'
+import WhatsNewPage from './components/pages/WhatsNew.vue'
+
+import HubRelease from './hub-release'
 
 interface IPageButton {
 	to: string
@@ -20,8 +47,11 @@ export interface IPage {
 export default [
 	{
 		id: 'key-aspects',
-		component: KeyNotes,
+		component: KeyNotesPage,
 		buttons: [{
+			to: 'whats-new',
+			label: t('firstrunwizard', 'What\'s new?'),
+		}, {
 			to: 'devices',
 			label: t('firstrunwizard', 'Nextcloud on all your devices'),
 		}],
@@ -29,7 +59,7 @@ export default [
 
 	{
 		id: 'devices',
-		component: DeviceIntegration,
+		component: DeviceIntegrationPage,
 		buttons: [{
 			to: 'about',
 			label: t('firstrunwizard', 'More about Nextcloud'),
@@ -38,11 +68,38 @@ export default [
 
 	{
 		id: 'about',
-		component: AboutNextcloud,
+		component: AboutNextcloudPage,
+		buttons: [{
+			to: 'hub-release',
+			label: t('firstrunwizard', 'Nextcloud Hub {version}', { version: HubRelease.version }),
+		}],
+	},
+
+	{
+		id: 'hub-release',
+		component: HubReleasePage,
+		buttons: [{
+			to: 'whats-new',
+			label: t('firstrunwizard', 'Read more'),
+		}],
+	},
+
+	{
+		id: 'whats-new',
+		component: WhatsNewPage,
+		buttons: [{
+			to: 'share',
+			label: t('firstrunwizard', 'Share on social media'),
+		}],
+	},
+
+	{
+		id: 'share',
+		component: SharePage,
 		buttons: [{
 			to: 'close',
 			label: t('firstrunwizard', 'Get started!'),
 		}],
-	}
+	},
 
 ] as IPage[]
