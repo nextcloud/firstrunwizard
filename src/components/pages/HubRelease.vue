1daa1bfb15c3fab13d9a124f0e333311418bbeca
Ferdinand Thiessen <opensource@fthiessen.de>
2024-03-27 14:43:10 +0100

    feat: Implemented Nextcloud Hub changelog
    This adds additional slides to the wizard for changes in the current Nextcloud Hub release
    and for sharing it with other users.
    Also modified the menu entry from "About" to "About & What's new".
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ src/components/pages/HubRelease.vue
@@ -0,0 +1,59 @@
+<!--
+  - @copyright Copyright (c) 2024 Ferdinand Thiessen <opensource@fthiessen.de>
+  -
+  - @author Ferdinand Thiessen <opensource@fthiessen.de>
+  -
+  - @license AGPL-3.0-or-later
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template>
+	<WizardPage :title="t('firstrunwizard', 'Introducing Nextcloud Hub {version}', { version: HUBRelease.version })">
+		<a :href="HUBRelease.link" :class="$style.anchor" target="_blank">
+			<video :class="$style.animation"
+				playsinline
+				autoplay
+				muted>
+				<source :src="videoWebm" type="video/webm">
+				<source :src="videoMp4" type="video/mp4">
+				{{ HUBRelease.videoAltText }}
+			</video>
+		</a>
+	</WizardPage>
+</template>
+
+<script setup lang="ts">
+import { translate as t } from '@nextcloud/l10n'
+import { imagePath } from '@nextcloud/router'
+
+import HUBRelease from '../../hub-release'
+import WizardPage from '../WizardPage.vue'
+
+const videoMp4 = imagePath('firstrunwizard', 'nextcloudHub.mp4')
+const videoWebm = imagePath('firstrunwizard', 'nextcloudHub.webm')
+</script>
+
+<style module>
+.anchor {
+	width: 100%;
+}
+
+.animation {
+	border-radius: var(--border-radius-rounded);
+	object-fit: contain;
+	width: 100%;
+}
+</style>
---
+++ src/components/pages/SharePage.vue
@@ -0,0 +1,111 @@
+<!--
+  - @copyright Copyright (c) 2024 Ferdinand Thiessen <opensource@fthiessen.de>
+  -
+  - @author Ferdinand Thiessen <opensource@fthiessen.de>
+  -
+  - @license AGPL-3.0-or-later
+  -
+  - This program is free software: you can redistribute it and/or modify
+  - it under the terms of the GNU Affero General Public License as
+  - published by the Free Software Foundation, either version 3 of the
+  - License, or (at your option) any later version.
+  -
+  - This program is distributed in the hope that it will be useful,
+  - but WITHOUT ANY WARRANTY; without even the implied warranty of
+  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  - GNU Affero General Public License for more details.
+  -
+  - You should have received a copy of the GNU Affero General Public License
+  - along with this program. If not, see <http://www.gnu.org/licenses/>.
+  -
+  -->
+
+<template>
+	<WizardPage :title="t('firstrunwizard', 'Find our more on the blog')">
+		<NcButton :href="HubRelease.link" target="_blank">
+			{{ t('firstrunwizard', 'Read the Nextcloud Hub {version} blog', { version: HubRelease.version}) }} ↗
+		</NcButton>
+
+		<section :class="$style.share_section">
+			<h2>{{ t('firstrunwizard', 'Share your opinion about Nextcloud Hub {version}', { version: HubRelease.version }) }}</h2>
+			<div :class="$style.share_wrapper">
+				<Card v-for="entry of shareLinks"
+					:key="entry.id"
+					:class="$style.card"
+					:href="entry.link"
+					:title="entry.id === 'mail' ? t('firstrunwizard', 'Share via email') : t('firstrunwizard', 'Share on {socialMedia}', { socialMedia: entry.name })">
+					<NcIconSvgWrapper v-if="entry.icon"
+						:svg="entry.id !== 'email' ? entry.icon : undefined"
+						:path="entry.id === 'email' ? entry.icon : undefined" />
+				</Card>
+			</div>
+		</section>
+	</WizardPage>
+</template>
+
+<script setup lang="ts">
+import { mdiEmail } from '@mdi/js'
+import { translate as t } from '@nextcloud/l10n'
+import { NcButton, NcIconSvgWrapper } from '@nextcloud/vue'
+
+import facebookSvg from '../../../img/facebook.svg?raw'
+import mastodonSvg from '../../../img/mastodon.svg?raw'
+import xSvg from '../../../img/x.svg?raw'
+
+import HubRelease from '../../hub-release'
+import WizardPage from '../WizardPage.vue'
+import Card from '../Card.vue'
+
+const encodedLink = encodeURIComponent(encodeURI(HubRelease.link))
+const mailSubject = encodeURIComponent(HubRelease.shareSubject ?? t('firstrunwizard', 'Nextcloud Hub {version} release', { version: HubRelease.version }))
+const mailBody = encodeURIComponent(t('firstrunwizard', 'Read more about it on the Nextcloud Hub {version} blog', { version: HubRelease.version }) + '\n' + HubRelease.link)
+
+const shareLinks = [
+	{
+		id: 'facebook',
+		name: 'Facebook',
+		link: `https://www.facebook.com/sharer/sharer.php?u=${encodedLink}`,
+		icon: facebookSvg,
+	},
+	{
+		id: 'x',
+		name: 'X',
+		link: `https://x.com/intent/post?url=${encodedLink}&via=Nextclouders&text=${encodeURIComponent(HubRelease.shareSubject ?? '')}`,
+		icon: xSvg,
+	},
+	{
+		id: 'mastodon',
+		name: 'Mastodon',
+		link: `https://mastodon.social/share?text=${encodedLink}`,
+		icon: mastodonSvg,
+	},
+	{
+		id: 'email',
+		name: 'EMail',
+		link: `mailto:?subject=${mailSubject}&body=${mailBody}`,
+		icon: mdiEmail,
+	},
+]
+</script>
+
+<style module>
+.share_section {
+	width: 100%;
+}
+
+.share_wrapper {
+	display: flex;
+	flex-direction: row;
+	flex-wrap: wrap;
+	gap: calc(var(--default-grid-baseline) * 4);
+	justify-content: space-around;
+	width: 100%;
+}
+
+.card {
+	flex: 1 1 auto;
+
+	/* Reduce padding a bit as we only have a single line of text with icon */
+	padding: calc(var(--default-grid-baseline) * 2) !important;
+}
+</style>
