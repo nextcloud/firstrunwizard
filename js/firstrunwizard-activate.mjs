6e49abac4d425a8bb01ee7cfe409d7ad508fd4f3
nextcloud-command <nextcloud-command@users.noreply.github.com>
2024-04-23 08:34:25 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/firstrunwizard-activate.mjs
+++ js/firstrunwizard-activate.mjs
@@ -1,3 +1,3 @@
-const __vite__fileDeps=[OC.filePath('firstrunwizard', '', 'js/main-TLYkAnT0.mjs'),OC.filePath('firstrunwizard', '', 'css/firstrunwizard-main.css')],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
+const __vite__fileDeps=[OC.filePath('firstrunwizard', '', 'js/main-CezsHINt.mjs'),OC.filePath('firstrunwizard', '', 'css/firstrunwizard-main.css')],__vite__mapDeps=i=>i.map(i=>__vite__fileDeps[i]);
 /*! third party licenses: js/vendor.LICENSE.txt */
-import{_ as t}from"./modulepreload-polyfill-DDskOgo1.mjs";document.addEventListener("DOMContentLoaded",async function(){(await t(()=>import("./main-TLYkAnT0.mjs"),__vite__mapDeps([0,1]),import.meta.url)).open()});
+import{_ as t}from"./modulepreload-polyfill-DDskOgo1.mjs";document.addEventListener("DOMContentLoaded",async function(){(await t(()=>import("./main-CezsHINt.mjs"),__vite__mapDeps([0,1]),import.meta.url)).open()});
