6046e84f5cc20b1640d24ca24d398684ce0a9556
Andy Scherzinger <info@andy-scherzinger.de>
2024-05-08 17:37:32 +0200

    fix: remove whitespace at the end of the copyright line
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * SPDX-FileCopyrightText: 2016 Nextcloud GmbH and Nextcloud contributors
- * SPDX-FileCopyrightText: 2014 ownCloud, Inc. 
+ * SPDX-FileCopyrightText: 2014 ownCloud, Inc.
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 return [
