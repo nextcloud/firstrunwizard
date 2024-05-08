6e78c1f1188106e89f8182750fd866087a76a4a1
Ferdinand Thiessen <opensource@fthiessen.de>
2024-05-09 01:43:01 +0200

    fix: Add license information to all test and config files
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,3 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2016 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: CC0-1.0
+ */
 const stylelintConfig = require('@nextcloud/stylelint-config')
 
 module.exports = stylelintConfig
