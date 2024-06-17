8285f84bb47059f7400f8f61a3c9e345df4d62e2
Ferdinand Thiessen <opensource@fthiessen.de>
2024-06-17 13:40:55 +0200

    fix: Migrate personal settings to Vue
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

---
+++ src/components/settings/SettingsServer.vue
@@ -0,0 +1,62 @@
+<!--
+  - SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+<template>
+	<NcSettingsSection :name="t('firstrunwizard', 'Server address')"
+		:description="t('firstrunwizard', 'Use this link to connect your apps and desktop client to this server:')">
+		<NcInputField id="endpoint-url"
+			:class="$style.input"
+			:label="t('firstrunwizard', 'Server address')"
+			show-trailing-button
+			readonly
+			:value="url"
+			@trailing-button-click="onCopyContent">
+			<template #trailing-button-icon>
+				<NcIconSvgWrapper :path="copyIcon" />
+			</template>
+		</NcInputField>
+	</NcSettingsSection>
+</template>
+
+<script setup lang="ts">
+import { mdiCheck, mdiContentCopy } from '@mdi/js'
+import { showError, showSuccess } from '@nextcloud/dialogs'
+import { translate as t } from '@nextcloud/l10n'
+import { getBaseUrl } from '@nextcloud/router'
+import { ref } from 'vue'
+
+import NcIconSvgWrapper from '@nextcloud/vue/dist/Components/NcIconSvgWrapper.js'
+import NcInputField from '@nextcloud/vue/dist/Components/NcInputField.js'
+import NcSettingsSection from '@nextcloud/vue/dist/Components/NcSettingsSection.js'
+
+const url = getBaseUrl()
+
+const copyIcon = ref(mdiContentCopy)
+const pendingTimeout = ref(0)
+
+/**
+ * Copy the base URL to the clipboard
+ */
+function onCopyContent() {
+	try {
+		window.navigator.clipboard.writeText(url)
+		showSuccess(t('firstrunwizard', 'URL copied'))
+		// Reset pending resets
+		if (pendingTimeout.value) {
+			window.clearTimeout(pendingTimeout.value)
+		}
+		// Update the icon to check to show success
+		copyIcon.value = mdiCheck
+		pendingTimeout.value = window.setTimeout(() => { copyIcon.value = mdiContentCopy }, 3000)
+	} catch (error) {
+		showError(t('firstrunwizard', 'Could not copy the URL, please copy manually'))
+	}
+}
+</script>
+
+<style module>
+.input {
+	max-width: min(50vw, 450px) !important;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,15 @@ import { join } from 'node:path'
 export default createAppConfig({
 	activate: join(__dirname, 'src', 'first-run.js'),
 	about: join(__dirname, 'src', 'app-menu.js'),
+	settings: join(__dirname, 'src', 'personal-settings.ts'),
 }, {
+	extractLicenseInformation: {
+		includeSourceMaps: true,
+		overwriteLicenses: {
+			'@nextcloud/axios': 'GPL-3.0-or-later',
+		},
+	},
+	thirdPartyLicense: false,
 	config: {
 		build: {
 			// Just one CSS files for now
