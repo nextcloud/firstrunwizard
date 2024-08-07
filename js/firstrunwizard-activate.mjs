82147416a6b7fe970fc22dad249fe33b0852d3a4
Ferdinand Thiessen <opensource@fthiessen.de>
2024-08-07 15:46:40 +0200

    chore: Compile assets
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- js/firstrunwizard-activate.mjs
+++ js/firstrunwizard-activate.mjs
@@ -1,3 +1,2 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('firstrunwizard', '', 'js/main.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'css/firstrunwizard-main.css')])))=>i.map(i=>d[i]);
-/*! third party licenses: js/vendor.LICENSE.txt */
-import{_ as t}from"./modulepreload-polyfill.chunk.mjs";document.addEventListener("DOMContentLoaded",async function(){(await t(()=>import("./main.chunk.mjs"),__vite__mapDeps([0,1]),import.meta.url)).open()});
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('firstrunwizard', '', 'js/main-Bs75fxKL.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'js/NcIconSvgWrapper-Ckqpz-vm-DfxD4hqH.chunk.mjs')])))=>i.map(i=>d[i]);
+import{_ as t}from"./modulepreload-polyfill-xeIZn2tR.chunk.mjs";document.addEventListener("DOMContentLoaded",async function(){(await t(()=>import("./main-Bs75fxKL.chunk.mjs"),__vite__mapDeps([0,1]),import.meta.url)).open()});
