152c0bcd6218abee9eab4326cd38c1c29e04fb73
Ferdinand Thiessen <opensource@fthiessen.de>
2025-05-06 14:25:09 +0200

    chore: update to ESLint v9 and matching Nextcloud provided config
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-declare module '@nextcloud/vue/dist/Components/*' {
+declare module '@nextcloud/vue/components/*' {
 	import type { Component } from 'vue'
 
 	const vue: Component
