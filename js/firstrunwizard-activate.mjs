b1e2a483188451ecae60b1c8221089c8629ed60f
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-09-03 17:46:14 +0000

    chore(assets): Recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/firstrunwizard-activate.mjs
+++ js/firstrunwizard-activate.mjs
@@ -1,3 +1,3 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('firstrunwizard', '', 'js/main-BvEdaccD.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'js/NcModal-CdXZncEM-CISbQOmN.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'js/index-DADYeDSW.chunk.mjs')])))=>i.map(i=>d[i]);
-import{_ as t}from"./modulepreload-polyfill-CBWYtosv.chunk.mjs";document.addEventListener("DOMContentLoaded",async function(){(await t(()=>import("./main-BvEdaccD.chunk.mjs"),__vite__mapDeps([0,1,2]),import.meta.url)).open()});
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('firstrunwizard', '', 'js/main-Cvux2CB1.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'js/NcModal-CdXZncEM-dCzgSTfD.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'js/index-DADYeDSW.chunk.mjs')])))=>i.map(i=>d[i]);
+import{_ as t}from"./modulepreload-polyfill-CBWYtosv.chunk.mjs";document.addEventListener("DOMContentLoaded",async function(){(await t(()=>import("./main-Cvux2CB1.chunk.mjs"),__vite__mapDeps([0,1,2]),import.meta.url)).open()});
 //# sourceMappingURL=firstrunwizard-activate.mjs.map
