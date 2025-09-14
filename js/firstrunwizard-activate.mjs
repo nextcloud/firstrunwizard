efafe639aff312a5159ef8a046571d6d6e3ec8b2
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-09-14 03:13:52 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

--- js/firstrunwizard-activate.mjs
+++ js/firstrunwizard-activate.mjs
@@ -1,3 +1,3 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('firstrunwizard', '', 'js/main-BN-dkV0e.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'js/NcModal-vyUsS1qu-BOPnIq6D.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'js/index-DfQpeuDm.chunk.mjs')])))=>i.map(i=>d[i]);
-import{_ as t}from"./modulepreload-polyfill-CBWYtosv.chunk.mjs";document.addEventListener("DOMContentLoaded",async function(){(await t(()=>import("./main-BN-dkV0e.chunk.mjs"),__vite__mapDeps([0,1,2]),import.meta.url)).open()});
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=[window.OC.filePath('firstrunwizard', '', 'js/main-O2UtDsxC.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'js/NcModal-vyUsS1qu-BOPnIq6D.chunk.mjs'),window.OC.filePath('firstrunwizard', '', 'js/index-DfQpeuDm.chunk.mjs')])))=>i.map(i=>d[i]);
+import{_ as t}from"./modulepreload-polyfill-CBWYtosv.chunk.mjs";document.addEventListener("DOMContentLoaded",async function(){(await t(()=>import("./main-O2UtDsxC.chunk.mjs"),__vite__mapDeps([0,1,2]),import.meta.url)).open()});
 //# sourceMappingURL=firstrunwizard-activate.mjs.map
