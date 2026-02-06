44fb2faa7f3178453a7caa55ce6798fcf05cdbf9
Ferdinand Thiessen <opensource@fthiessen.de>
2026-02-06 16:26:18 +0100

    chore: lint typescript and resolve issues found
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- src/vite.d.ts
+++ src/vite.d.ts
@@ -3,9 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-declare module '@nextcloud/vue/components/*' {
-	import type { Component } from 'vue'
-
-	const vue: Component
-	export default vue
+declare module '*?raw' {
+	const content: string
+	export default content
 }
